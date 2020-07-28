570f424366a4eac2b29092866854d874d1bbff91
Shawn Sherwood <ssherwood@vmware.com>
2020-07-28 12:50:30 -0400

    Fixing manifest and some defaults

--- src/main/java/io/undertree/evaadapter/api/WebhookTargetTestController.java
+++ src/main/java/io/undertree/evaadapter/api/WebhookTargetTestController.java
@@ -3,6 +3,7 @@ package io.undertree.evaadapter.api;
 import io.undertree.evaadapter.models.WebexTeamsMarkdown;
 import io.undertree.evaadapter.models.WebexTeamsText;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookTargetTestController {
 
-    @PostMapping("/test/webex-text")
-    public void webhookTargetTest(@RequestBody WebexTeamsText message) {
-        log.info("Incoming Message = " + message);
-        return;
+    @PostMapping("/v1/webhooks/incoming/webex-text")
+    public ResponseEntity<?> webhookTargetTest(@RequestBody WebexTeamsText message) {
+        log.info("Incoming message: " + message);
+        log.warn("If you are *NOT* testing, alerts are not being routed properly!!!  Please set the WEBHOOK_TARGET_URL.");
+        return ResponseEntity.ok("");
     }
 
-    @PostMapping("/test/webex-markdown")
-    public void webhookTargetTest(@RequestBody WebexTeamsMarkdown message) {
-        log.info("Incoming Message = " + message);
-        return;
+    @PostMapping("/v1/webhooks/incoming/webex-markdown")
+    public ResponseEntity<?> webhookTargetTest(@RequestBody WebexTeamsMarkdown message) {
+        log.info("Incoming message: " + message);
+        log.warn("If you are *NOT* testing, alerts are not being routed properly!!!  Please set the WEBHOOK_TARGET_URL.");
+        return ResponseEntity.ok("");
     }
 }
