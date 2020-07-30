24491fc5da7e7643933cb0bdee53e433c7e5e984
Shawn Sherwood <ssherwood@vmware.com>
2020-07-30 08:49:53 -0400

    Changed handling of metadata since discovering it is just implemented as a map

--- src/main/java/io/undertree/evaadapter/models/EventAlert.java
+++ src/main/java/io/undertree/evaadapter/models/EventAlert.java
@@ -2,12 +2,14 @@ package io.undertree.evaadapter.models;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class EventAlert {
     String publisher;
     String topic;
     String timestamp;
+    Map<String, String> metadata;
     String subject;
     String body;
-    EventAlertMetadata metadata;
 }
